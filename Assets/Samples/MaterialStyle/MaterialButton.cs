c9b17ba5d2fe55aeab6161d021416dd2d58f6d2f
Xue <339131001@qq.com>
2018-07-26 09:44:07 +0800

    material style button

---
+++ Assets/Samples/MaterialStyle/MaterialButton.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MaterialButton : MonoBehaviour,
+    IPointerDownHandler,
+    IPointerUpHandler,
+    IPointerExitHandler
+{
+    private Vector2 point;
+    private Material mat;
+    private float radius = 0;
+    private bool isUp;
+
+    void Awake()
+    {
+        mat = new Material(Shader.Find("Custom/Wave"));
+        GetComponent<Image>().material = mat;
+    }
+
+    IEnumerator normal()
+    {
+        while (true)
+        {
+            if (isUp) break;
+
+            //size expand in normal speed
+            radius *= 1.05f;
+            mat.SetFloat("_Radius", radius);
+
+            //pass vector4 of mousePosition to shader
+            Vector4 v4 = new Vector4(point.x, point.y, 0, 0);
+            mat.SetVector("_Point1", v4);
+
+            if (radius == 0) break;
+
+            yield return null;
+        }
+    }
+
+    IEnumerator fast()
+    {
+        float timer = 0; 
+        //enlarge wave for 1 sec
+        while (timer < 1)
+        {
+            timer += Time.deltaTime;
+
+            //size expand faster than normal speed
+            radius *= 1.5f;
+            mat.SetFloat("_Radius", radius);
+
+            //color fade
+            Vector4 col = new Vector4(1, timer, timer, 1);
+            mat.SetVector("_Color", col);
+
+            yield return null;
+        }
+        //reset
+        isUp = false;
+        radius = 0;
+        mat.SetFloat("_Radius", radius);
+    }
+
+    #region Interface
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData data)
+    {
+        //when new wave begin, avoid confict with the old one
+        StopAllCoroutines();
+
+        isUp = false;
+
+        //size and position of the new wave
+        radius = 500;
+        point = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+
+        //color
+        Vector4 col = new Vector4(1, 0, 0, 1);
+        mat.SetVector("_Color", col);
+
+        StartCoroutine(normal());
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData data)
+    {
+        isUp = true; //to break normal coroutine
+        StartCoroutine(fast());
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData data)
+    {
+        StartCoroutine(fast());
+    }
+
+#endregion 
+
+}
---
+++ Assets/Samples/MaterialStyle/Wave.shader
@@ -0,0 +1,50 @@
+﻿Shader "Custom/Wave"   
+{
+    Properties
+    {
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_Color("Color", Color) = (1, 0, 0, 1)
+        _Point1("Point1", vector) = (100, 100, 0, 0)
+		_Radius("Radius", range(0, 50000)) = 0
+    }
+    SubShader
+    {
+        Pass
+		{    
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {    
+                float4 vertex : POSITION;
+            };
+            struct v2f
+            {    
+                float4 vertex : SV_POSITION;
+            };
+			
+            float4 _Color;
+            float4 _Point1;
+			float _Radius;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+            fixed4 frag (v2f i) : SV_Target
+            {
+                if(pow((i.vertex.x - _Point1.x), 2) + pow((i.vertex.y - _Point1.y), 2) < _Radius) //set area to draw wave
+                {
+					return _Color; //set wave color
+                }
+                return fixed4(1, 1, 1, 1); //set background color
+            }
+            ENDCG
+        }
+    }
+}
